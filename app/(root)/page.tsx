7dd283d7b1e3ce78967eb17acba2628571f15763
Leon Bryan <leonard.bryan.email@gmail.com>
2025-09-15 20:50:35 +0800

    Added different Parallax library

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,20 @@
-
-
-import BackgroundImg from "../components/BackgroundImg";
+import ParallaxWrapper from "../components/ParallaxWrapper";
 import MainCard from "../components/MainCard";
-
-
 
 export default function Home() {
   return (
-    <div className="h-screen relative z-0">
-      <div className="">
-        <BackgroundImg />
-      </div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+    <div>
+      <ParallaxWrapper>
+        <div data-depth="0.6">
+          <div className="bg-[url(/imgs/DSCF0121.JPG)] blur-[3px] brightness-80 bg-center bg-cover bg-no-repeat w-screen h-screen"></div>
+        </div>
+        <div data-depth="0.2"  className="h-screen w-screen border-2 flex justify-center items-center">
+          <MainCard />
+        </div>
+      </ParallaxWrapper>
+      {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
         <MainCard />
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ app/components/ParallaxWrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import Parallax from "parallax-js";
+import { useEffect, useRef } from "react";
+
+interface ParallaxWrapperProps {
+  children: React.ReactNode;
+}
+
+const ParallaxWrapper = ({ children }: ParallaxWrapperProps) => {
+  const sceneRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!sceneRef.current) return;
+
+    const parallaxInstance = new Parallax(sceneRef.current, {
+      relativeInput: true, // example option
+    });
+
+    return () => {
+      parallaxInstance.destroy();
+    };
+  }, []);
+
+  return (
+    <div ref={sceneRef}>{children}</div>
+
+    // <div
+    //   globalFactorX={0.1}
+    //   globalFactorY={0.1}
+    //   className="bg-[url(/imgs/DSCF0121.JPG)] blur-[2px] brightness-80 bg-center bg-cover bg-no-repeat w-screen h-screen"
+    // >
+    //   <MouseParallaxChild factorX={0.3} factorY={0.5}>
+    //     <div className="bg-[url(/imgs/DSCF0121.JPG)] blur-[3px] brightness-80 bg-center bg-cover bg-no-repeat w-screen h-screen"></div>
+    //   </MouseParallaxChild>
+    // </div>
+  );
+};
+
+export default ParallaxWrapper;
