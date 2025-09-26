ee42193d752730e812e6ac4e218e251f0474e994
Leon Bryan <leonard.bryan.email@gmail.com>
2025-09-27 05:09:57 +0800

    Extra fixes on the player

--- app/utils/hooks/useAudioPlayer.ts
+++ app/utils/hooks/useAudioPlayer.ts
@@ -11,64 +11,55 @@ export const useAudioPlayer = () => {
   // This feature is too difficult to implement oh Lawd.
   // const FADE_DURATION = 1500;
 
-  //   const getTrack = (index: number, shouldPlay = false): void => {};
-
-  //JANKY
-  const getTrack = (index: number, autoPlay = false) => {
-    soundRef.current?.stop();
-    soundRef.current?.unload();
-
-    const newSound = new Howl({
+  useEffect(() => {
+    const sound = new Howl({
       src: [tracks[trackIndex].src],
       html5: true,
       volume: 0.2,
-      onend: () => {
-        const next = index + 1 + tracks.length;
-        setTrackIndex(next);
-        getTrack(next, true);
-      },
+      onend: () => setTrackIndex((prev) => (prev + 1) % tracks.length),
     });
 
-    soundRef.current = newSound;
-    setTrackIndex(index);
+    soundRef.current?.unload();
+    soundRef.current = sound;
 
-    if (autoPlay) {
-      newSound.play();
-      setPlaying(true);
-    } else {
-      setPlaying(false);
+    if (shouldPlay) {
+      sound.play();
     }
-  };
 
+    return () => {
+      sound.unload();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [trackIndex, shouldPlay]);
+
+  //Next
   const nextTrack = () => {
     //JANKY
-    const next = (trackIndex + 1) % tracks.length;
-    getTrack(next, isPlaying);
+    setTrackIndex((prev) => (prev + 1) % tracks.length);
+    setShouldPlay(true);
   };
 
+  //Prev
   const prevTrack = () => {
     //JANKY
-    const next = (trackIndex - 1 + tracks.length) % tracks.length;
-    getTrack(next, isPlaying);
+    setTrackIndex((prev) => (prev - 1 + tracks.length) % tracks.length);
+    setShouldPlay(true);
   };
 
+  //Play
   const play = () => {
     setPlaying(true);
     soundRef.current?.play();
   };
+
+  //Pause
   const pause = () => {
     setPlaying(false);
     soundRef.current?.pause();
   };
 
+  //Pause
   const stop = () => soundRef.current?.stop();
-
-  useEffect(() => {
-    getTrack(trackIndex, true);
-    return () => {
-      soundRef.current?.unload();
-    };
-  }, []);
 
   return {
     track: tracks[trackIndex],
